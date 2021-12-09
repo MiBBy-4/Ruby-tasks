30fb0e6efa17818571f6e35779a0b545e4366ead
MiBBy-4 <maksim.762373maks@bk.ru>
2021-12-09 15:50:28 +0300

    feat: changes in exersice 1

--- exercise1/exercise1.rb
+++ exercise1/exercise1.rb
@@ -1,11 +1,11 @@
-def get_sum
-    sum = 0
 
-    (1..1000).each do |item_of_array|
+def sum
+
+    (1..1000).reduce(0) {|sum, item_of_array|
         sum += item_of_array if item_of_array % 3 == 0 or item_of_array % 5 == 0 
-    end
+        sum
+    }
 
-    sum
 end
 
-p get_sum
+p sum
--- exercise1/spec/exercise1_spec.rb
+++ exercise1/spec/exercise1_spec.rb
@@ -1,8 +1,8 @@
 require 'rspec'
 require'../exercise1'
 
-describe get_sum do
+describe "sum" do
     it "is sum of numbers below 1000 that multiplies of 3 or 5" do
-        expect(get_sum).to be 234168     
+        expect(sum).to be 234168     
     end
 end
