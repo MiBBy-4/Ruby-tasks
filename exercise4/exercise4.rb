bfff8d6562909608bb645baa307194f0ce2cdbf7
MiBBy-4 <maksim.762373maks@bk.ru>
2021-12-10 18:09:46 +0300

    fix: added consts in exercise 4, refactor: added spaces and empty line in exercise4 files

--- exercise4/exercise4.rb
+++ exercise4/exercise4.rb
@@ -1,6 +1,9 @@
 # минимальное число при умножении трехзначных чисел - 10000(100*100), максимальное - 998001(999*999)
+MinNumber = 10000
+MaxNumber = 998001
+
 def find_palindroms
-  (10000..998001).select{|number| number if number.to_s == number.to_s.reverse}
+  (MinNumber..MaxNumber).select { |number| number if number.to_s == number.to_s.reverse } 
 end
 
 def find_largest_palindrom
