aaf910b6e7538cb3acd6d374896a9d79b19734a6
MiBBy-4 <maksim.762373maks@bk.ru>
2021-12-10 17:59:40 +0300

    fix: added number in const and moved it out of the func. refactor: added empty line in the end of file

--- exercise2/exercise2.rb
+++ exercise2/exercise2.rb
@@ -1,10 +1,12 @@
 #0,1,1,2,3,5,8,13,21,.. <4000000
 
+Number = 4000000
+
 def fib
   first_elem, second_elem = 0, 1 # начальные элементы для сложения
   fib_sequence = []
 
-  while first_elem < 4000000 # пока наш первый элемент
+  while first_elem < Number # пока наш первый элемент
     fib_sequence << first_elem # помещаем элемент в массив
     first_elem, second_elem = second_elem, first_elem + second_elem # в первый элемент помещаем второй для того, чтобы в следующей итерации поместить его в массив, а во второй массив помещаем сумму первого и второго
   end
