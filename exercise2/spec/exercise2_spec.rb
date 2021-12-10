c7571ab42803758c878d426f74041b03a3111640
MiBBy-4 <maksim.762373maks@bk.ru>
2021-12-10 10:02:04 +0300

    refactor: add whitespace in second line in spec file

--- exercise2/spec/exercise2_spec.rb
+++ exercise2/spec/exercise2_spec.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-require ' ../exercise2'
+require '../exercise2'
 
 describe 'sum_of_even_fib' do
   it 'is sum of elements of Fibonacci sequence whose values do not exceed four million that are even' do
