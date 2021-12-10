8435baca334ca6c15da5e85c44456923f2c6b58c
MiBBy-4 <maksim.762373maks@bk.ru>
2021-12-10 10:27:31 +0300

    fix: changed tabs length in spec file

--- exercise3/spec/exercise3_spec.rb
+++ exercise3/spec/exercise3_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require '../exercise3'
 
 describe 'largest_of_factors' do
-    it 'is the largest prime factor of 13195' do
-        expect(largest_of_factors).to be 29
-    end
+  it 'is the largest prime factor of 13195' do
+      expect(largest_of_factors).to be 29
+  end
 end
