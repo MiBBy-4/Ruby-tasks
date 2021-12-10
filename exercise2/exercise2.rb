12c98ffc44a63c0162ff62d1430de1006e726d75
MiBBy-4 <maksim.762373maks@bk.ru>
2021-12-10 09:13:53 +0300

    refact: changed lenght of tabs in exercise 1 and exercise 2

--- exercise2/exercise2.rb
+++ exercise2/exercise2.rb
@@ -1,24 +1,24 @@
 #0,1,1,2,3,5,8,13,21,.. <4000000
 
 def fib
-    first_elem, second_elem = 0, 1 # начальные элементы для сложения
-    fib_sequence = []
+  first_elem, second_elem = 0, 1 # начальные элементы для сложения
+  fib_sequence = []
 
-    while first_elem < 4000000 # пока наш первый элемент
-        fib_sequence << first_elem # помещаем элемент в массив
-        first_elem, second_elem = second_elem, first_elem + second_elem # в первый элемент помещаем второй для того, чтобы в следующей итерации поместить его в массив, а во второй массив помещаем сумму первого и второго
-    end
+  while first_elem < 4000000 # пока наш первый элемент
+    fib_sequence << first_elem # помещаем элемент в массив
+    first_elem, second_elem = second_elem, first_elem + second_elem # в первый элемент помещаем второй для того, чтобы в следующей итерации поместить его в массив, а во второй массив помещаем сумму первого и второго
+  end
 
-    fib_sequence
+  fib_sequence
 end
 
 def sum_of_even_fib
-    fib_sequence = fib
+  fib_sequence = fib
 
-    fib_sequence.reduce(0) {|sum, number| 
-        sum += number if number.even?
-        sum
-    }
+  fib_sequence.reduce(0) {|sum, number| 
+      sum += number if number.even?
+      sum
+  }
 end
 
 puts sum_of_even_fib
--- exercise2/spec/exercise2_spec.rb
+++ exercise2/spec/exercise2_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require ' ../exercise2'
 
 describe 'sum_of_even_fib' do
-    it 'is sum of elements of Fibonacci sequence whose values do not exceed four million that are even' do
-        expect(sum_of_even_fib).to be 4613732
-    end
+  it 'is sum of elements of Fibonacci sequence whose values do not exceed four million that are even' do
+      expect(sum_of_even_fib).to be 4613732
+  end
 end
