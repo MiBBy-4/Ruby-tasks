8061ed3bc31f7fbb4ad248be5011ec4761c07e79
MiBBy-4 <maksim.762373maks@bk.ru>
2021-12-13 12:09:00 +0300

    refactor: change case of consts in exercise 6

--- exercise6/exercise6.rb
+++ exercise6/exercise6.rb
@@ -1,12 +1,12 @@
-MinNumber = 1
-MaxNumber = 100
+MIN_NUMBER = 1
+MAX_NUMBER = 100
 
 def sum_of_squares
-  (MinNumber..MaxNumber).reduce(0) { |sum, number| sum+=number**2 }
+  (MIN_NUMBER..MAX_NUMBER).reduce(0) { |sum, number| sum+=number**2 }
 end
 
 def square_of_sum
-  (MinNumber..MaxNumber).reduce(0) { |sum, number| sum+=number }**2
+  (MIN_NUMBER..MAX_NUMBER).reduce(0) { |sum, number| sum+=number }**2
 end
 
 def difference
