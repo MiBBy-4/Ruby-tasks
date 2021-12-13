bda31982848922e363d82db102d233ebd1a1942f
MiBBy-4 <maksim.762373maks@bk.ru>
2021-12-13 12:03:29 +0300

    refactor: change curky braces on do...end

--- exercise1/exercise1.rb
+++ exercise1/exercise1.rb
@@ -1,8 +1,8 @@
 def sum
-  (1..1000).reduce(0) { |sum, item_of_array|
+  (1..1000).reduce(0) do |sum, item_of_array|
     sum += item_of_array if item_of_array % 3 == 0 or item_of_array % 5 == 0 
     sum
-  }
+  end
 end
 
 p sum
