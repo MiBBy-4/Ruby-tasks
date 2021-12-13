d139b13462daa88355bc3d4bde70f98bf45b23c3
MiBBy-4 <maksim.762373maks@bk.ru>
2021-12-13 12:14:51 +0300

    refactor: change case of consts in exercise 5 file

--- exercise5/exercise5.rb
+++ exercise5/exercise5.rb
@@ -1,12 +1,12 @@
-MaxNumber = 10000
-LastNumber = 10
+MAX_NUMBER = 10000
+LAST_NUMBER = 10
 
 def smallest_number
   smallest_number = 0
-  (1..MaxNumber).each do |number|
-    (1..LastNumber).each do |divider|
+  (1..MAX_NUMBER).each do |number|
+    (1..LAST_NUMBER).each do |divider|
       break if number % divider != 0
-      smallest_number =  number if divider == LastNumber
+      smallest_number =  number if divider == LAST_NUMBER
     end
     break if smallest_number != 0
   end
