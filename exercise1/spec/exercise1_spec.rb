8b40133b4d00296df2edb43dbbdbfaa4171991a9
MiBBy-4 <maksim.762373maks@bk.ru>
2021-12-13 13:59:08 +0300

    refactor: change size of tabs in exercise1 files

--- exercise1/spec/exercise1_spec.rb
+++ exercise1/spec/exercise1_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require '../exercise1'
 
 describe 'sum' do
-    it 'is sum of numbers below 1000 that multiplies of 3 or 5' do
-        expect(sum).to be 234168     
-    end
+  it 'is sum of numbers below 1000 that multiplies of 3 or 5' do
+      expect(sum).to be 234168     
+  end
 end
