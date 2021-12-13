1c59a600903bf889152d90566a07bf6fc2eee9d3
MiBBy-4 <maksim.762373maks@bk.ru>
2021-12-13 14:05:52 +0300

    refactor: move arguments of the loops up

--- exercise9/exercise9.rb
+++ exercise9/exercise9.rb
@@ -3,10 +3,8 @@ SUM = 1000
 def product
   product = 0
 
-  (1..SUM).each do # here we iterate values for a
-    |a|
-    (a+1..SUM-1).each do # here we iterate values for b
-      |b|
+  (1..SUM).each do |a| # here we iterate values for a
+    (a+1..SUM-1).each do |b| # here we iterate values for b
       c = ((a**2 + b**2).to_f)**0.5 # here we get the values of c by the formula: c^2 = a^2 + b^2 => c = (a^2 + b^2)^1/2 
       if a + b + c == SUM
         product = a * b * c # here we get the product if a + b + c = 1000 and break our loop
