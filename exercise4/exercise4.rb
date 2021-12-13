d927fe818fde961b85a3a62b2fb987081369ce42
MiBBy-4 <maksim.762373maks@bk.ru>
2021-12-13 12:07:18 +0300

    refactor: change case of consts in exercise file

--- exercise4/exercise4.rb
+++ exercise4/exercise4.rb
@@ -1,9 +1,9 @@
 # минимальное число при умножении трехзначных чисел - 10000(100*100), максимальное - 998001(999*999)
-MinNumber = 10000
-MaxNumber = 998001
+MIN_NUMBER = 10000
+MAX_NUMBER = 998001
 
 def find_palindroms
-  (MinNumber..MaxNumber).select { |number| number if number.to_s == number.to_s.reverse } 
+  (MIN_NUMBER..MAX_NUMBER).select { |number| number if number.to_s == number.to_s.reverse } 
 end
 
 def find_largest_palindrom
